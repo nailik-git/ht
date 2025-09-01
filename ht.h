72853841b5669afd6dc0fe85333a7fbbded05edf
NailikGit <kilian.smeets@web.de>
2025-09-01 21:30:19 +0200

    add prefix functionality

--- ht.h
+++ ht.h
@@ -1,6 +1,7 @@
 #ifndef HT_H
 #define HT_H
 
+
 #include <assert.h>
 #include <errno.h>
 #include <stdint.h>
@@ -30,6 +31,40 @@ static_assert(0, "CMP not defined, expected function signature: int (*)(KEY_TYPE
 #define POP 0.75
 #endif
 
+#undef KEY_VALUE
+#undef HASH_TABLE
+#undef HT_INIT
+#undef HT_DEINIT
+#undef HT_INSERT
+#undef HT_UPDATE
+#undef HT_FIND
+#undef HT_DELETE
+#undef __HT_REALLOC
+#ifdef PREFIX
+#define __concat(a, b) a ## b
+#define __concat2(a, b) __concat(a, b)
+#define __change_name(a) __concat2(PREFIX, a)
+#define KEY_VALUE __change_name(__concat2(_, KV))
+#define HASH_TABLE __change_name(__concat2(_, HT))
+#define HT_INIT __change_name(_ht_init)
+#define HT_DEINIT __change_name(_ht_deinit)
+#define HT_INSERT __change_name(_ht_insert)
+#define HT_UPDATE __change_name(_ht_update)
+#define HT_FIND __change_name(_ht_find)
+#define HT_DELETE __change_name(_ht_delete)
+#define __HT_REALLOC __change_name(__ht_realloc)
+#else
+#define KEY_VALUE KV
+#define HASH_TABLE HT
+#define HT_INIT ht_init
+#define HT_DEINIT ht_deinit
+#define HT_INSERT ht_insert
+#define HT_UPDATE ht_update
+#define HT_FIND ht_find
+#define HT_DELETE ht_delete
+#define __HT_REALLOC __ht_realloc
+#endif
+
 // -------------------------------------------------
 // --------------- type declarations ---------------
 // -------------------------------------------------
@@ -38,23 +73,23 @@ typedef struct {
   KEY_TYPE key;
   VAL_TYPE val;
   char occupied;
-} KV;
+} KEY_VALUE;
 
 typedef struct {
-  KV* items;
+  KEY_VALUE* items;
   size_t count;
   size_t capacity;
-} HT;
+} HASH_TABLE;
 
 // -------------------------------------------------
 // ------------- function declarations -------------
 // -------------------------------------------------
 
 // creates a new hash table with initial size.
-HT ht_init(size_t inital_size);
+HASH_TABLE HT_INIT(size_t inital_size);
 
 // frees all memory associated with hash table
-void ht_deinit(HT ht);
+void HT_DEINIT(HASH_TABLE ht);
 
 // insert key-value pair with key and val,
 // if an item with the same key exists, its value will be overwritten.
@@ -62,24 +97,24 @@ void ht_deinit(HT ht);
 // if the hash table is full, it will reallocate, this is quite expensive,
 // to counteract this, just increase the initial size in ht_init,
 // or define NO_RESIZE, the hash table will then just error.
-void ht_insert(HT* ht, KEY_TYPE key, VAL_TYPE val);
+void HT_INSERT(HASH_TABLE* ht, KEY_TYPE key, VAL_TYPE val);
 
 // get a reference to the key-value pair with the corresponding key,
 // if it does not exist, a new item is inserted.
-KV* ht_update(HT* ht, KEY_TYPE key);
+KEY_VALUE* HT_UPDATE(HASH_TABLE* ht, KEY_TYPE key);
 
 // get a reference to the key-value pair with the corresponding key,
 // if it does not exist, this function returns NULL.
-KV* ht_find(HT ht, KEY_TYPE key);
+KEY_VALUE* HT_FIND(HASH_TABLE ht, KEY_TYPE key);
 
 // deletes key-value pair with corresponding key.
-void ht_delete(HT* ht, KEY_TYPE key);
+void HT_DELETE(HASH_TABLE* ht, KEY_TYPE key);
 
 
 // internal function, should not be used,
 // changes the size of ht, according to s,
 // re-inserts all items.
-void __ht_realloc(HT* ht, size_t s);
+void __HT_REALLOC(HASH_TABLE* ht, size_t s);
 
 // -------------------------------------------------
 // ------------ function implementation ------------
@@ -87,10 +122,10 @@ void __ht_realloc(HT* ht, size_t s);
 
 #ifndef HT_NO_IMPLEMENTATION
 
-HT ht_init(size_t initial_size) {
-  HT ht = {0};
+HASH_TABLE HT_INIT(size_t initial_size) {
+  HASH_TABLE ht = {0};
   ht.capacity = initial_size;
-  ht.items = calloc(initial_size, sizeof(KV));
+  ht.items = calloc(initial_size, sizeof(KEY_VALUE));
   if(!ht.items) {
     fprintf(stderr, "ERROR: memory allocation failed: %s", strerror(errno));
   }
@@ -98,12 +133,12 @@ HT ht_init(size_t initial_size) {
   return ht;
 }
 
-void ht_deinit(HT ht) {
+void HT_DEINIT(HASH_TABLE ht) {
   free(ht.items);
 }
 
-void ht_insert(HT* ht, KEY_TYPE key, VAL_TYPE val) {
-  if(ht->count >= ht->capacity * POP) __ht_realloc(ht, ht->capacity * 2);
+void HT_INSERT(HASH_TABLE* ht, KEY_TYPE key, VAL_TYPE val) {
+  if(ht->count >= ht->capacity * POP) __HT_REALLOC(ht, ht->capacity * 2);
 
   uint64_t h = HASH(key) % ht->capacity;
 
@@ -115,8 +150,8 @@ void ht_insert(HT* ht, KEY_TYPE key, VAL_TYPE val) {
   ht->count++;
 }
 
-KV* ht_update(HT* ht, KEY_TYPE key) {
-  if(ht->count >= ht->capacity * POP) __ht_realloc(ht, ht->capacity * 2);
+KEY_VALUE* HT_UPDATE(HASH_TABLE* ht, KEY_TYPE key) {
+  if(ht->count >= ht->capacity * POP) __HT_REALLOC(ht, ht->capacity * 2);
 
   uint64_t h = HASH(key) % ht->capacity;
 
@@ -134,7 +169,7 @@ KV* ht_update(HT* ht, KEY_TYPE key) {
   assert(0 && "unreachable");
 }
 
-KV* ht_find(HT ht, KEY_TYPE key) {
+KEY_VALUE* HT_FIND(HASH_TABLE ht, KEY_TYPE key) {
   uint64_t h = HASH(key) % ht.capacity;
 
   for(size_t i = 0; i < ht.capacity; i++) {
@@ -146,24 +181,24 @@ KV* ht_find(HT ht, KEY_TYPE key) {
   return NULL;
 }
 
-void ht_delete(HT* ht, KEY_TYPE key) {
-  KV* kv = ht_find(*ht, key);
+void HT_DELETE(HASH_TABLE* ht, KEY_TYPE key) {
+  KEY_VALUE* kv = HT_FIND(*ht, key);
   kv->occupied = 0;
   ht->count--;
 
   if(ht->count < ht->capacity / 2 * POP) {
-    __ht_realloc(ht, ht->capacity / 2);
+    __HT_REALLOC(ht, ht->capacity / 2);
   }
 }
 
-void __ht_realloc(HT *ht, size_t s) {
+void __HT_REALLOC(HASH_TABLE *ht, size_t s) {
 #ifdef NO_RESIZE
   fprintf(stderr, "ERROR: hashtable full\n");
   exit(1);
 #else // NO_RESIZE
-  KV* old_items = ht->items;
+  KEY_VALUE* old_items = ht->items;
   size_t old_capacity = ht->capacity;
-  ht->items = calloc(s, sizeof(KV));
+  ht->items = calloc(s, sizeof(KEY_VALUE));
   ht->capacity = s;
   for(size_t i = 0; i < old_capacity; i++) {
     if(old_items[i].occupied) {
